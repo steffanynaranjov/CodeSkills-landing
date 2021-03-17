df4d1b9b26144c28deda5fdc0a6eee3386473d47
steffanynaranjov <steffany.naranjo@gmail.com>
2021-03-17 13:22:48 -0500

    starting the page

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss({
+      content: ["./**/*.html"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
